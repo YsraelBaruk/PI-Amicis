1d0c6b9bf712ebfe8caa843d55e9c00263f5a373
israel <israekbaruque992@gmail.com>
2024-04-24 16:52:13 -0300

    branch de teste

--- app/Http/Controllers/Firebase/UserContoller.php
+++ app/Http/Controllers/Firebase/UserContoller.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Firebase;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Contract\Firestore;
 
 class UserContoller extends Controller
 {
+    public function __construct(Firestore $firestore)
+    {
+        $this->firestore = $firestore;
+    }
+
     public function login(){
         return View('login.index');
     }
@@ -15,5 +21,13 @@ class UserContoller extends Controller
         return View('login.home');
     }
 
-    
+    public function createAccount(Request $request){
+        $postData = [
+            'firstName' => $request->first_name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => $request->password,
+        ];
+        $postRef = $database->getReference('posts')->push($postData);
+    }
 }
